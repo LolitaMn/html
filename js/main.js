2b63591d38fbc1a116bf29a97ab21877ba755db6
LolitaMn <lolita.mkhitarian@gmail.com>
2021-10-03 15:23:24 +0400

    added some scripts

--- js/main.js
+++ js/main.js
@@ -55,8 +55,92 @@ console.log(arr.length);
 
 */
 
-var matrix = [                             // это многомерные массивы
+/*var matrix = [                             // это многомерные массивы
 [4, 6, 8], ["stroka", 5.7,], [0, -100]
 ];
 matrix[1][0] = 90;
-console.log(matrix);
+console.log(matrix);  
+*/
+
+
+
+/*for(var i  = 100; i > 5; i /=2) {    // Для перебора массивов. В циклах работают с числами. Фиг скобки не обязательны при ед., но всегда прописывают
+console.log(i)
+}
+*/
+
+/*var j = 1000;                             
+while(j > 100) {                          // While для проверки некого условия. Фигурные скобки обязательны
+console.log(j);
+    j-=100;
+
+}
+
+
+var isHasCar = true;
+while(isHasCar == true) {         // это бесконечный цикл так как условие удовлетвореноб не запускать бесконечный цикл так как комп зависнет. 
+}
+*/
+
+
+
+/*var x = 0;
+do {                             // do while когда нужно сначала выполнить потом проверить условие.
+console.log(x);
+x++;
+} while(x < 10);
+*/
+
+
+/*for(var i = 10; i <= 20; i+=2) {
+    if(i > 15)
+    break;
+    
+    console.log(i);
+}
+*/
+
+
+/*for(var i = 10; i <= 20; i++) {
+    if(i % 2 == 0)
+    continue;                                  // условный оператор в циклах
+    
+    console.log(i);
+} */
+
+
+/*var arr = [5, 7, 3, 8, 9, 91];
+
+for(var i = 0; i < arr.length; i++) {                        // перебор массива
+    arr[i] *= 2;
+    console.log("Элемент: " + (i + 1) + ": " + arr[i]);
+} 
+*/
+
+
+//alert("Погода норм");
+//confirm("Вы дома?");
+
+/*var  data = confirm("Вы дома");
+console.log(data); */
+
+/*var  data = confirm("Вы дома?");
+if(data) {
+    alert("вы молодец!");
+}
+*/
+
+//prompt("Сколько вам лет,скажите, 20");
+
+/*var data = prompt("Сколько вам лет, скажите");
+console.log(data);
+*/
+
+var person = null;
+if(confirm("Вы уверены?")) {
+person = prompt("введите ваше имя");
+alert("Привет, " + person);
+} else {
+    alert("Вы нажали отмена");
+
+}
