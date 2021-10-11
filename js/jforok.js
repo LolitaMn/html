5df3dd14a7e8e3ae437e661cacde98bfdf44cd84
Lolita Mkhitaryan <LolitaM@ogma.local>
2021-10-11 18:38:42 +0400

    all

--- js/jforok.js
+++ js/jforok.js
@@ -72,7 +72,7 @@ function purchase() {
     errorP.textContent = "Something went wrong!"
 }
 */
-
+/*
 let num1 = 8
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
@@ -104,3 +104,77 @@ add()
 substract()
 divide()
 multiply()
+*/
+/*
+let firstCard = 1
+let secondCard = 20
+
+let sum = firstCard + secondCard
+console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want another card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You won!")
+} else {
+    console.log("You're out")
+}
+
+*/
+/*
+let age = 100
+console.log(age)
+
+if(age < 100) {
+    console.log("Not eligible")                         /// === или == это равно и не путать с = 
+} else if (age === 100) {
+console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not eligible, you have already gotten one")
+}
+
+*/
+
+/*
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard 
+let hasBlackJack = false
+let isAlive = true
+
+if(false) {
+    console.log("Do you want to draw a new card?")
+    
+} else if (true) {
+    console.log("Wohoo! You've got Blackjack!")
+    
+} else  {
+    console.log("You're out of the game")
+}
+
+*/
+
+
+
+let firstCard = 10
+let secondCard = 9
+let sum = firstCard + secondCard + 2
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+function startGame() {
+if(sum <= 20) {
+    message = "Do you want to draw a new card"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game"
+    isAlive = false
+}
+
+console.log(message)
+}
+
+
