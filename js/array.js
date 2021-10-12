c39d9e8c19d3cbf0fe1e522e2a7ff1cc34014b6a
Lolita Mkhitaryan <LolitaM@ogma.local>
2021-10-12 18:44:37 +0400

    arrays also

---
+++ js/array.js
@@ -0,0 +1,29 @@
+let featuredPosts = [
+    "Check my netflix code",          //0
+    "here's the code of my project",   //1
+    "i've just relauched my portfolio"  //2
+]
+/*
+console.log(featuredPosts[0])
+console.log(featuredPosts[1])
+console.log(featuredPosts[2])
+
+console.log(featuredPosts.length)
+*/
+
+let me = ["it specialis", 29, true]
+
+let cards = [7, 4]
+
+cards.push(5)
+//console.log(cards)
+
+let message = ["hey, bro", "hy you too", "ok!"]
+
+let newMessage = "Same here!"
+
+message.push(newMessage)
+console.log(message)
+
+message.pop()
+console.log(message)
--- js/jforok.js
+++ js/jforok.js
@@ -158,23 +158,44 @@ if(false) {
 
 let firstCard = 10
 let secondCard = 9
-let sum = firstCard + secondCard + 2
+let cards = [firstCard, secondCard]      // array ordered list of items
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-function startGame() {
+let messageEl = document.getElementById("message-el")
+//let sumEl = document.getElementById("sum-el") or так как ниже более динамично
+ let sumEl = document.querySelector(".sum-el")
+ let cardsEl = document.querySelector("#cards-el")
+
+ function startGame(){
+      renderGame()
+ }
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
 if(sum <= 20) {
     message = "Do you want to draw a new card"
 } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack!"
     hasBlackJack = true
+    
 } else {
     message = "You're out of the game"
     isAlive = false
+    
+}
+ 
+ messageEl.textContent = message
 }
 
-console.log(message)
+function newCard() {
+    console.log("Drawing a new card from the deck")
+    let card = 2
+    sum += card
+    renderGame()
 }
 
 
