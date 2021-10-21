444743803912c4fc4e214462f649f7ae1e7ba5fa
Lolita Mkhitaryan <LolitaM@ogma.local>
2021-10-21 18:35:06 +0400

    ok1

--- js/c_extension.js
+++ js/c_extension.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+console.log( localStorage.getItem("myLeads"))
 
 inputBtn.addEventListener("click",function() {
     myLeads.push(inputEl.value)
